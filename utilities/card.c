5d5fe93db71f148bac631872b583635d8077526e
PassardiEnea <eneapassardi@gmail.com>
2025-11-25 09:40:20 +0100

    aggiornamento e introduzione di alcune funzionalità

--- utilities/card.c
+++ utilities/card.c
@@ -1,8 +1,9 @@
 #include "generic.c"
 #include <time.h>
+#include <stdint.h>
 
 // Definizione della card 
-struct {
+struct card {
     // Campo ID: gestito in modo incrementale e identificato da un intero
     int id;
 
@@ -13,21 +14,30 @@ struct {
     char* testo_attivita;
 
     // Campo relativo agli utenti, cioè il loro numero di porta, che hanno implmentata e completata
-    int utente_creatore;
-    int utente_assegnatario;
+    uint16_t utente_creatore;
+    uint16_t utente_assegnatario;
 
     // Campo Timestamp: rappresenta il momento in cui la card è stata modificata l'ultima volta
-    // TODO --> Decidere il tipo da utilizzare
-    /**
-     * [YYYY-MM-DD HH:MM:SS] --> Formato standard per rappresentare data e ora
-     * si potrebbe usare una struttura apposita con dei campi int e una funzione che permette
-     * di formattare la data e l'ora in questo formato.
-     */
-
     time_t ultimo_aggiornamento;
     
-} card; 
+}; 
 
 // Ridefinizione del tipo stuct card così da semplificarne la scrittura
 typedef struct card card_t;
+
+
+// Funzione per creare una nuova card
+card_t * crea_card(int id, colonna_t colonna, const char* testo_attivita, uint16_t utente_creatore, uint16_t utente_assegnatario) {
+    card_t *nuova_card = (card_t *)malloc(sizeof(card_t));
+    if (nuova_card == NULL) {
+        return NULL; // Gestione errore allocazione memoria
+    }
+    nuova_card->id = id;
+    nuova_card->colonna = colonna;
+    nuova_card->testo_attivita = strdup(testo_attivita);
+    nuova_card->utente_creatore = utente_creatore;
+    nuova_card->utente_assegnatario = utente_assegnatario;
+    nuova_card->ultimo_aggiornamento = time(NULL);
+    return nuova_card;
+}
 
--- utilities/generic.c
+++ utilities/generic.c
@@ -4,27 +4,3 @@ typedef enum colonna colonna_t;
 
 
 
-// DEFINIZIONE DELLA STRUTTURA TIMESTAMP
-//------------------------------------------------------------
-struct {
-    int anno;
-    int mese;
-    int giorno;
-    int ora;
-    int minuto;
-    int secondo;
-} timestamp;
-
-typedef struct timestamp timestamp_t;
-
-// FUNZIONI PER LA GESTIONE DEL TIMESTAMP
-//------------------------------------------------------------
-/*void aggiorna_timestamp(int anno, int mese, int giorno, int ora, int minuto, int secondo) {
-    timestamp_t* ts = (timestamp_t*) malloc(sizeof(timestamp_t));
-    ts->anno = anno;
-    ts->mese = mese;
-    ts->giorno = giorno;
-    ts->ora = ora;
-    ts->minuto = minuto;
-    ts->secondo = secondo;
-}*/
