d559d4607aa89068f09fa732a10e53e4ef503faf
Passa230 <eneapassardi@gmail.com>
2025-11-27 15:53:45 +0100

    aggiunta della gestione della mutua esclusione nella generazione dell'ID delle card.

--- lib/generic.c
+++ lib/generic.c
@@ -1,5 +1,14 @@
-unsigned int get_new_id(){
-    // TODO: prendere un semaforo per poter incrementare
-    // l'idenificatore dell'ID in mutua esclusione
+#include "generic.h"
+
+/* Definizioni delle variabili dichiarate come extern in generic.h */
+unsigned int NEXT_CARD_ID = 1;
+pthread_mutex_init(semaforo_ID);
+
+unsigned int get_new_id(void){
+    unsigned int id;
+    pthread_mutex_lock(&semaforo_ID);
+    id = NEXT_CARD_ID++;
+    pthread_mutex_unlock(&semaforo_ID);
+    return id;
 }
 
--- lib/generic.h
+++ lib/generic.h
@@ -1,19 +1,23 @@
-#ifdef GENERIC_H
+#ifndef GENERIC_H
 #define GENERIC_H
 
+#include <pthread.h>
+
+// Gestione dei booleani
 #define TRUE    1
 #define FALSE   0
 
 typedef unsigned short bool_t;
 
+
+// Gestione degli stati di una card
 enum colonna {TO_DO, DOING, DONE};
 
 typedef enum colonna colonna_t;
 
+// Dichiarazioni per la generazione degli id (definizioni in generic.c)
+extern unsigned int NEXT_CARD_ID;
+extern pthread_mutex_t semaforo_ID;
+unsigned int get_new_id(void);
 
-unsigned int NEXT_CARD_ID = 0;
-unsigned int get_new_id();
-
-
-
-#endif
+#endif /* GENERIC_H */
