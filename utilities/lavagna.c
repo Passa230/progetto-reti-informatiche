5a4357f97a853f3c4c190e09bed98235b27b101e
Passa230 <enea.passardi@gmail.com>
2025-12-18 16:39:18 +0100

    refactor

--- utilities/lavagna.c
+++ utilities/lavagna.c
@@ -214,6 +214,46 @@ void lavagna_stampa(char* buf, size_t max_len){
         used += written;
         list = list->next_card;
     }
+
+    written = snprintf(
+        buf, 
+        max_len, 
+        "|------------------------------|\n|-------------DOING------------|\n|------------------------------|\n"
+    );
+
+    used += written;
+
+    card_t* list = lavagna.cards[1];
+    while (list != NULL) {
+        written = snprintf(
+            buf + used,
+            max_len -used,
+            "  - %s",
+            list->testo_attivita
+        );
+        used += written;
+        list = list->next_card;
+    }
+
+    written = snprintf(
+        buf, 
+        max_len, 
+        "|------------------------------|\n|-------------DONE-------------|\n|------------------------------|\n"
+    );
+
+    used += written;
+
+    card_t* list = lavagna.cards[2];
+    while (list != NULL) {
+        written = snprintf(
+            buf + used,
+            max_len -used,
+            "  - %s",
+            list->testo_attivita
+        );
+        used += written;
+        list = list->next_card;
+    }
     
     
     for (int i = 0; i < 3; i++) {
